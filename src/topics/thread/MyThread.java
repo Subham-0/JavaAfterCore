5e6d8cdcfbfcbcdc4a3810ce4a27e7d2668a0a24
Subham-0 <subhamd2022@gift.edu.in>
2025-12-07 11:26:36 +0530

    thread states

--- src/topics/thread/MyThread.java
+++ src/topics/thread/MyThread.java
@@ -3,11 +3,22 @@ package topics.thread;
 public class MyThread extends Thread {
     @Override
     public void run() {
-
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    static void main(String[] args) {
+    static void main(String[] args) throws InterruptedException {
         MyThread t1 = new MyThread();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
+        Thread.sleep(1000);
+        System.out.println(t1.getState());
+        t1.join(); //main method wait until t1 complete it task
         System.out.println(t1.getState());
     }
 }
