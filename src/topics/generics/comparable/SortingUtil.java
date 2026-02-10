1d1bfa6853864cece2205a696c59345680c1730b
Subham-0 <subhamd2022@gift.edu.in>
2026-02-10 14:05:43 +0530

    concept: Generics with Comparable

--- src/topics/generics/comparable/SortingUtil.java
+++ src/topics/generics/comparable/SortingUtil.java
@@ -39,7 +39,7 @@ public class SortingUtil{
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (list.get(j).compareTo(list.get(j + 1)) > 0) {
-                    // Swap
+                    // swap
                     T temp = list.get(j);
                     list.set(j, list.get(j + 1));
                     list.set(j + 1, temp);
